4673c319d72cf825bc484b01ae7c1fe265549424
Pedro Almeida <59484474+PedroMarianoAlmeida@users.noreply.github.com>
2020-07-08 07:56:30 -0300

    Image adjust

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,12 @@
 import Head from 'next/head'
-import {Container, Row, Col, Carousel} from 'reactstrap'
+import { Container, Row, Col, Carousel } from 'reactstrap'
 
 export default function Home() {
   return (
     <div className="container">
       <Head>
         <title>Caio Andrade</title>
-        <meta name="description" content="The best music projects of Caio Andrade"/>
+        <meta name="description" content="The best music projects of Caio Andrade" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
@@ -21,19 +21,23 @@ export default function Home() {
                 <Col xs="12">
                   <button className="btn btn-light border border-dark px-2 py-3 mt-2"><span className="lead">Discover Project</span></button>
                 </Col>
-              </Row>        
+              </Row>
             </Col>
 
             <Col xs="12" md="8">
               <div className="h-100">
                 <img src="/images/image-project-1.PNG" alt="Project 1 Image" className="img-thumbnail my-image" />
-                <div className="my-content ml-3 d-md-none d-block">
-                  <div className="display-4 ">Caio Project 1</div>
-                  <button className="btn btn-light border border-dark px-2 py-3 mt-2"><span className="lead">Discover Project</span></button>
-                </div>             
+                <div className="my-content ml-3 d-md-none">
+                  <div className="display-4 my-text">Caio Project 1</div>
+                  <br />
+                  <div>
+                    <button className="btn btn-light border border-dark px-1 py-2 my-button"><span className="lead">Discover Project</span></button>
+                  </div>
+                
               </div>
-              
-              <style jsx>{`
+              </div>
+
+            <style jsx>{`
                 .my-image {
                   position: relative;
                   z-index: 0;
@@ -42,7 +46,21 @@ export default function Home() {
                 .my-content {
                   position: absolute;
                   z-index: 1;
-                  top: 37%;
+                  top: 25%;
+                }
+
+                .my-text {
+                  font-size: 2rem;                 
+                }
+
+                .my-button {
+                  font-size: 0.5rem;
+                }
+
+                @media only screen and (max-width: 992px) {
+                  .my-image {
+                    filter: opacity(50%);
+                  }
                 }
               `}</style>
             </Col>
@@ -50,7 +68,7 @@ export default function Home() {
         </Container>
       </main>
 
-      
-    </div>
+
+    </div >
   )
 }
