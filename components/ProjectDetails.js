5e1620ae8fda1ff7335ae85eea7014238b722b3b
Pedro Almeida <59484474+PedroMarianoAlmeida@users.noreply.github.com>
2020-07-22 14:25:28 -0300

    Update ProjectDetails.js
    Adjust to mobile version

--- components/ProjectDetails.js
+++ components/ProjectDetails.js
@@ -13,8 +13,22 @@ const ProjectDetails = (props) => {
                     <p>{props.text[language]}</p>
                 </Col>
 
-                <Col xs='12' md='6'>
-                    <iframe width="560" height="315" src={props.adress} frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen className="rounded border"></iframe>
+                <Col xs='12' md='6' className="my-1">
+                    <iframe src={props.adress} frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen id="my-video"></iframe>
+                    <style jsx>{`
+                    #my-video {
+                        width: 560px;
+                        height: 315px;                        
+                    }
+
+                    @media only screen and (max-width: 992px) {
+                        #my-video {
+                            width: 100%;
+                            
+                        }
+                      }
+                    `}</style>
+                
                 </Col>
 
                 <Col xs='12' md='6' className={props.textLocation === "left" ? "d-flex d-md-none align-items-center" : "d-flex align-items-center"}>
