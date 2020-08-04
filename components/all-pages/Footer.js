1d41c0db893767730d9c8b481dcfa4845130d6e3
Pedro Almeida <59484474+PedroMarianoAlmeida@users.noreply.github.com>
2020-08-03 23:26:27 -0300

    Inserting FaceAwsome logos components

--- components/all-pages/Footer.js
+++ components/all-pages/Footer.js
@@ -1,64 +1,27 @@
+import { faFacebookSquare, faInstagramSquare, faYoutubeSquare } from '@fortawesome/free-brands-svg-icons'
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+
 const Footer = () => {
   return (
     <footer className="fixed-bottom bg-white d-flex justify-content-center" id='my-footer'>
 
-      <a href="https://www.facebook.com/CaiaoReliquia" target="_blank" className="mx-4 py-2">
-        <div className="mx-4" id="facebook" alt='Caio-Andrade-facebook-profile'></div>
+      <a href="https://www.facebook.com/CaiaoReliquia" target="_blank" className="mx-4 my-1">
+        <FontAwesomeIcon icon={faFacebookSquare} className="mx-1 text-secondary" alt="Facebook-logo" size="2x" />
       </a>
 
-      <a href="https://www.instagram.com/caioandrademix/" target="_blank" className="mx-4 py-2 ">
-        <div className="mx-4" id="instagram" alt='Caio-Andrade-instagram-profile'></div>
+      <a href="https://www.instagram.com/caioandrademix/" target="_blank" className="mx-4 my-1 ">
+        <FontAwesomeIcon icon={faInstagramSquare} className="mx-1 text-secondary" alt="Instagram-logo" size="2x" />
       </a>
 
-      <a href="https://www.youtube.com/channel/UCTGXBtyqOjyV0R78iUAFFhg" target="_blank" className="mx-4 py-2 " >
-        <div className="mx-4" id="youtube" alt='Caio-Andrade-youtube-channel'></div>
+      <a href="https://www.youtube.com/channel/UCTGXBtyqOjyV0R78iUAFFhg" target="_blank" className="mx-4 my-1 " >
+        <FontAwesomeIcon icon={faYoutubeSquare} className="mx-1 text-secondary" alt="Youtube-logo" size="2x" />
       </a>
 
       <style jsx>{`
                 #my-footer{
-                    height: 7%;
                     box-shadow: 0 -10px 10px rgb(212 207 207);
                 }
-
-                #facebook {
-                    height: 100%;
-                    width: 100%;
-                    background-color: rgba(92 87 87);
-                    -webkit-mask-size: contain;
-                    mask-size: contain;
-                    -webkit-mask-position: center;
-                    mask-position: center;
-                    -webkit-mask-repeat: no-repeat;
-                    mask-repeat: no-repeat;
-                    mask-image: url("/images/company-logos/facebook.svg");
-                  }
-
-                  #instagram {
-                    height: 100%;
-                    width: 100%;
-                    background-color: rgba(92 87 87);
-                    -webkit-mask-size: contain;
-                    mask-size: contain;
-                    -webkit-mask-position: center;
-                    mask-position: center;
-                    -webkit-mask-repeat: no-repeat;
-                    mask-repeat: no-repeat;
-                    mask-image: url("/images/company-logos/instagram.svg");
-                  }
-
-                  #youtube {
-                    height: 100%;
-                    width: 100%;
-                    background-color: rgba(92 87 87);
-                    -webkit-mask-size: contain;
-                    mask-size: contain;
-                    -webkit-mask-position: center;
-                    mask-position: center;
-                    -webkit-mask-repeat: no-repeat;
-                    mask-repeat: no-repeat;
-                    mask-image: url("/images/company-logos/youtube.svg");
-                  }
-
             `}</style>
     </footer>
   );
