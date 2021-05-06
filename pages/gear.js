f64a100b0f5c8896abb21f9f8d9f4e5e398ece48
Pedro Almeida <59484474+PedroMarianoAlmeida@users.noreply.github.com>
2021-05-06 08:38:31 -0300

    Change gear and SoloArtists

--- pages/gear.js
+++ pages/gear.js
@@ -1,107 +1,111 @@
-import { useContext } from 'react';
-import { Container, Row, Col } from 'reactstrap';
+import { useContext } from "react";
+import { Container, Row, Col } from "reactstrap";
 
-import Layout from '../components/all-pages/Layout';
-import { LanguageContext } from '../contexts/LanguageContext';
+import Layout from "../components/all-pages/Layout";
+import { LanguageContext } from "../contexts/LanguageContext";
 
 const text = {
-    title: { eng: "Gear (Studio Setup)", pt: "Gear (equipamentos do estúdio)" },
-    item1: {
-        eng: "Yamaha NS10 Monitors",
-        pt: "Monitores Yamaha NS10"
+  title: { eng: "Gear (Studio Setup)", pt: "Gear (equipamentos do estúdio)" },
+  items: [
+    {
+      eng: "Yamaha NS10 Monitors",
+      pt: "Monitores Yamaha NS10",
     },
 
-    item2: {
-        eng: "Alesis RA150 Reference Amplifier",
-        pt: "Amplificador de Referencia  Alesis RA150"
+    {
+      eng: "Alesis RA150 Reference Amplifier",
+      pt: "Amplificador de Referencia  Alesis RA150",
     },
 
-    item3: {
-        eng: "Mackie 1604 – 16ch Analog mixing console",
-        pt: "Mesa Analogica Mackie 1604 – 16 Canais"
-    },
-    
-    item4: {
-        eng: "MOTU M828 MKII 8 to 16 channel Interface",
-        pt: "Interface MOTU M828 MKII"
-    },
-    
-    item5: {
-        eng: "Presonus EUREKA Channel Strip",
-        pt: "Channel Strip Presonus EUREKA"
+    {
+      eng: "Yamaha MC1602 – 16ch Analog mixing console",
+      pt: "Mesa Analogica Yamaha MC1602 – 16 Canais",
     },
 
-    item6: {
-        eng: "DBX 166 – Stereo VCA Compressor/Gate",
-        pt: "Compressor/Limiter VCA Stereo – DBX 166"
+    {
+      eng: "M-Audio ProFire2626 8 to 26 channel Interface",
+      pt: "Interface M-Audio ProFire 2626",
     },
 
-    item7: {
-        eng: "Presonus EQ3B – 3 band Parametric Equalizer",
-        pt: "Equalizador Paramétrico Presonus EQ3B"
+    {
+      eng: "Phonic Firefly ADA8 - 8 Channel AD/DA Converter",
+      pt: "Conversor AD/DA 8 canais - Phonic Firefly Ada8",
     },
 
-    item8: {
-        eng: "ART Tube EQ – 4 band Parametric Tube Equalizer",
-        pt: "Equalizador Paramétrico Valvulado ART TUBE EQ"
+    {
+      eng: "Motu 2408 - 8 Channel AD/DA Converte",
+      pt: "Conversor AD/DA 8 canais - Motu 2408",
     },
 
-    item9: {
-        eng: "Samson C-Comp – Opto Compressor",
-        pt: "Compressor Óptico Samson C-Comp"
+    {
+      eng: "Presonus EUREKA Channel Strip",
+      pt: "Channel Strip Presonus EUREKA",
     },
 
-    item10: {
-        eng: "DBX 266 XL – Stereo VCA Compressor/Gate",
-        pt: "Compressor/Limiter VCA Stereo  – DBX 266 XL"
+    {
+      eng: "Alesis 3630 – Stereo VCA Compressor/Gate",
+      pt: "Compressor/Limiter VCA Stereo – Alesis 3630",
     },
 
-    item11: {
-        eng: "Behringer T1952 – Two Channel Interactive Tube Compressor",
-        pt: "Compressor Valvulado Stereo – Behringer T1952"
+    {
+      eng: "Presonus Bluemax - VCA Stereo Buss Compressor",
+      pt: "Compressor Stereo Bus VCA - Presonus Bluemax",
     },
-
-    item12: {
-        eng: "Behringer ADA8000 – 8 channel AD/DA Conversor ",
-        pt: "Conversor AD/DA 8 Canais – Behringer ADA8000"
+    {
+      eng: "ART Tube EQ – 4 band Parametric Tube Equalizer",
+      pt: "Equalizador Paramétrico Valvulado ART TUBE EQ",
     },
-
-    item13: {
-        eng: "24 In/Out Half Normal Patchbay ",
-        pt: "Patchbay Half normal 24 In/out"
+    {
+      eng: "Samson C-Comp – Opto Compressor",
+      pt: "Compressor Óptico Samson C-Comp",
     },
-}
+    {
+      eng: "DBX 266 XL – Stereo VCA Compressor/Gate",
+      pt: "Compressor/Limiter VCA Stereo – DBX 266 XL",
+    },
+    {
+      eng: "Behringer T1952 – Two Channel Interactive Tube Compressor",
+      pt: "Compressor Valvulado Stereo – Behringer T1952",
+    },
+    {
+      eng: "Phonic DFX256 – Stereo MultifFX",
+      pt: "Máquina de efeitos Phonic DFX256",
+    },
+    {
+      eng: "24 In/Out Full Normal Patchbay",
+      pt: "Patchbay Full normal 24 In/out",
+    },
+  ],
+};
 
 const Gear = () => {
-    const { language } = useContext(LanguageContext);
+  const { language } = useContext(LanguageContext);
 
-    return (
-        <Layout seo={{ title: "Gear | Caio Andrade Mixing", description: "Caio's mixing setup", author: "Caio Andrade" }} className="h-100">
-            <Container className="mt-5">
-                <Row>
-                    <Col xs='12' className="my-5 pb-4">
-                        <h1>{text.title[language]}</h1>
-                        <ul className="list-group list-group-flush">
-                            <li className="list-group-item">{text.item1[language]}</li>
-                            <li className="list-group-item">{text.item2[language]}</li>
-                            <li className="list-group-item">{text.item3[language]}</li>
-                            <li className="list-group-item">{text.item4[language]}</li>
-                            <li className="list-group-item">{text.item5[language]}</li>
-                            <li className="list-group-item">{text.item6[language]}</li>
-                            <li className="list-group-item">{text.item7[language]}</li>
-                            <li className="list-group-item">{text.item8[language]}</li>
-                            <li className="list-group-item">{text.item9[language]}</li>
-                            <li className="list-group-item">{text.item10[language]}</li>
-                            <li className="list-group-item">{text.item11[language]}</li>
-                            <li className="list-group-item">{text.item12[language]}</li>
-                            <li className="list-group-item">{text.item13[language]}</li>
-                        </ul>
-                    </Col>
-                </Row>
-            </Container>
-        </Layout>
-    )
-
-}
+  return (
+    <Layout
+      seo={{
+        title: "Gear | Caio Andrade Mixing",
+        description: "Caio's mixing setup",
+        author: "Caio Andrade",
+      }}
+      className="h-100"
+    >
+      <Container className="mt-5">
+        <Row>
+          <Col xs="12" className="my-5 pb-4">
+            <h1>{text.title[language]}</h1>
+            <ul className="list-group list-group-flush">
+              {text.items.map((item) => (
+                <li className="list-group-item" key={item[language]}>
+                  {item[language]}
+                </li>
+              ))}
+            </ul>
+          </Col>
+        </Row>
+      </Container>
+    </Layout>
+  );
+};
 
 export default Gear;
--- pages/projects/solo-artists.js
+++ pages/projects/solo-artists.js
@@ -1,41 +1,82 @@
-import { useContext } from 'react';
-import { Container, Row, Col } from 'reactstrap'
+import { useContext } from "react";
+import { Container, Row, Col } from "reactstrap";
 
-import Layout from '../../components/all-pages/Layout'
-import ProjectDetails from '../../components/ProjectDetails';
-import { LanguageContext } from '../../contexts/LanguageContext';
+import Layout from "../../components/all-pages/Layout";
+import ProjectDetails from "../../components/ProjectDetails";
+import { LanguageContext } from "../../contexts/LanguageContext";
 
 const text = {
-  title: {eng: "Solo Artists", pt:"Artistas Solo"},
-  
+  title: { eng: "Solo Artists", pt: "Artistas Solo" },
+
   description: {
     eng: "There are some track I mixed for Solo Artists",
-    pt: "A seguir alguns trabalhos que mixei para Artistas Solo"
-  }
-}
+    pt: "A seguir alguns trabalhos que mixei para Artistas Solo",
+  },
+};
+
+const soloArtists = [
+  {
+    artist: "Canto Perene",
+    music: "Amago",
+    credits: {
+      eng: "Producer / Mixing / Mastering",
+      pt: "Produção, mixagem e masterização",
+    },
+    address: "https://www.youtube.com/embed/-iceQYkcdDI",
+  },
+
+  {
+    artist: "Canto Perene",
+    music: "Silenciar",
+    credits: { eng: "Mixing / Mastering", pt: "Mxagem e masterização" },
+    address: "https://www.youtube.com/embed/q9JmvHlWRtk",
+  },
+
+  {
+    artist: "Canto Perene",
+    music: "Mergulho em Mim",
+    credits: {
+      eng: "Mixing / Mastering",
+      pt: "Mixagem e Masterização",
+    },
+    address: "https://www.youtube.com/embed/KFciXC2ElrA",
+  },
+];
 
 const PopSingersongwriterPage = () => {
   const { language } = useContext(LanguageContext);
 
   return (
-    <Layout seo={{ title: "Caio Andrade Mixing | Solo Artists Projects", description: "Solo Artists mixing portfolio", author: "Pedro Almeida" }} className="h-100">
+    <Layout
+      seo={{
+        title: "Caio Andrade Mixing | Solo Artists Projects",
+        description: "Solo Artists mixing portfolio",
+        author: "Pedro Almeida",
+      }}
+      className="h-100"
+    >
       <Container className="my-5 pb-5">
         <Row className="mt-5">
-
-        <Col xs="12" className="align-items-center mt-5">
-            <h1 className='text-center'>{text.title[language]}</h1>
-            <p className='lead my-3 text-center'>{text.description[language]}</p>
+          <Col xs="12" className="align-items-center mt-5">
+            <h1 className="text-center">{text.title[language]}</h1>
+            <p className="lead my-3 text-center">
+              {text.description[language]}
+            </p>
           </Col>
 
-          <ProjectDetails  artist="Canto Perene" music="Amago" credits={{ eng: "Producer / Mixing / Mastering", pt: "Produção, mixagem e masterização" }} adress="https://www.youtube.com/embed/-iceQYkcdDI"/>
-
-          <ProjectDetails artist="Canto Perene" music="Silenciar" credits={{ eng: "Mixing / Mastering", pt: "Mxagem e masterização" }} adress="https://www.youtube.com/embed/q9JmvHlWRtk"/>
-
+          {soloArtists.map((artist) => (
+            <ProjectDetails
+              artist={artist.artist}
+              music={artist.music}
+              credits={artist.credits}
+              adress={artist.address}
+              key={artist.music}
+            />
+          ))}
         </Row>
-
       </Container>
     </Layout>
   );
-}
+};
 
 export default PopSingersongwriterPage;
